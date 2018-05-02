0133f6b8dd5a9deb78f31980d80d033e3b72fef9
Lizzy <lizzy.brooks@gmail.com>
2018-05-02 14:11:14 -0700

    cleaned fft sketch

--- examples/fft-example/sketch.js
+++ examples/fft-example/sketch.js
@@ -49,27 +49,5 @@ function draw() {
     fill("pink");
     ellipse(100,100,100,100);
   }
-  // else{
-  //   background(0);
-  // }
-
-
-  // translate(width / 2, height / 2);
-  // //beginShape();
-  // for (var i = 0; i < spectrum.length; i++) {
-  //   var angle = map(i, 0, spectrum.length, 0, 360);
-  //   var amp = spectrum[i];
-  //   var r = map(amp, 0, 256, 20, 100);
-  //   //fill(i, 255, 255);
-  //   var x = r * cos(angle);
-  //   var y = r * sin(angle);
-  //   stroke(i, 255, 255);
-  //   line(0, 0, x, y);
-  //   //vertex(x, y);
-  //   //var y = map(amp, 0, 256, height, 0);
-  //   //rect(i * w, y, w - 2, height - y);
-  // }
-  // //endShape();
-
-
+  
 }
