d8e296478bacf03945b0d3a9319bf030879602a8
Lizzy Brooks <lizzy.brooks@gmail.com>
2018-10-02 09:36:09 -0700

    Add files via upload

---
+++ examples/image-manipulation/sketch.js
@@ -0,0 +1,28 @@
+//baby pic used in class example is by ms. brooks. cite url for pics found online
+let img;
+let colorTest;
+
+//preload will load before setup runs
+function preload() {
+  img = loadImage("images/jomyjunkyard.jpg");
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+  // background("white");
+  image(img, 100, 100,250,350); //draw the image to the canvas
+  colorTest = "black";
+
+}
+
+function draw() {
+ noStroke();
+ fill(colorTest);
+ rect(400,100,100,100);
+
+}
+
+function mouseClicked(){
+  colorTest = get(mouseX,mouseY);
+  print(colorTest);
+}
