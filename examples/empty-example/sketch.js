2eea2592946ae9b9dd276561cc61afa4fe513fea
Lizzy <lizzy.brooks@gmail.com>
2018-12-03 16:07:24 -0800

    debugged zach's game

--- examples/empty-example/sketch.js
+++ examples/empty-example/sketch.js
@@ -1,75 +1,109 @@
-function setup(){
-createCanvas(850,800);
-background(0,45,183);
+let me;
+let you;
+let gravity;
 
-squareGrid(10,10);
+let rectarray = [];
 
+let caught = false;
+
+
+function setup() {
+  frameRate(20);
+  createCanvas(1000,1000)
+  rectMode(CENTER)
+
+  me = new Rectangles
+  you = new Ball(height/2,20,3)
 }
+
 function draw() {
+  background(0,0,0)
 
-  //white background rectangle
-  noStroke();
-  fill(0,45,183);
-  rect(0, 0, 75, 20);
+  you.moveBall();
+  you.getCaught();
+  you.drawBall();
 
-  //text showing mouse coordinates
-  fill(255, 0, 0);
-  text("("+mouseX + ", " + mouseY+")", 5, 15);
+print(caught);
 
-  }
+  if (frameCount % 30 == 0) {
+      let  r = new Rectangles(random(0,width), height-30, 500, -4);
+      rectarray.push(r);
+      print(rectarray);
+    }
 
-  function squareGrid(rows,columns){
-    var maxRow = rows;
-    var maxCol = columns;
+  for (let i = 0; i < rectarray.length; i++) {
+  	 	rectarray[i].drawRect();
+      rectarray[i].moveRect();
+   }
+}
 
-    for (var row = 0; row <= maxRow; row++) {
-      push();
-      for (var col = 0; col <= maxCol; col++) {
+class Rectangles{
 
-        // var redAm = col/maxCol
-        var redAm = row/maxRow;
-        var blueAm = maxRow/row;
-        var greenAm = maxRow/row;
-
-          print(redAm);
-          print(blueAm);
-          print(greenAm);
-
-    //create a function for shaded cube grid
-      // repeat shaded cube grid function in a cube
-        // recurision
-
-        push();
-        if (row%2 != 0){
-          translate(35,0)
-            strokeWeight(0.5);
-              fill(redAm*10, greenAm*15, blueAm*100);
-            quad(20, 50, 55, 25, 90, 50, 55, 75);
-              fill(redAm*13, greenAm*71, blueAm*150);
-            quad(55, 75, 90, 50, 90, 90, 55, 115);
-              fill(redAm*42, greenAm*169, blueAm*211);
-            quad(20, 50, 55, 75, 55, 115, 20, 90);
-            // odd rows of pattern
-        pop();
-          translate (70,0);
-      } else {
-
-          strokeWeight(0.5);
-              fill(redAm*10, greenAm*15, blueAm*100);
-            quad(20, 50, 55, 25, 90, 50, 55, 75);
-              fill(redAm*13, greenAm*71, blueAm*150);
-            quad(55, 75, 90, 50, 90, 90, 55, 115);
-              fill(redAm*42, greenAm*169, blueAm*211);
-            quad(20, 50, 55, 75, 55, 115, 20, 90);
-            // even rows of pattern
-        pop();
-          translate (70,0);
+  constructor(x,y, width, speed){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.width = width;
       }
 
+	drawRect(){
+    		stroke(0);
+    		fill("white");
+		    rect(this.x,this.y,500,10);
+      }
+
+  moveRect(){
+    this.x;
+    this.y = this.y+ this.speed;
+  }
+}
+
+
+class Ball{
+
+  constructor(x,y, speed){
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+    }
+
+  drawBall(){
+      stroke(0);
+      fill("yellow");
+      ellipse(this.x,this.y,30,30);
+    }
+
+  moveBall(){
+     for (let i = 0; i < rectarray.length; i++) {
+
+        if (keyIsDown(65)){
+          this.x -= this.speed;
+        }
+
+        if (keyIsDown(68)){
+          this.x += this.speed;
+        }
+
+        if (this.x>0 && caught == false){
+          this.y= this.y+.5;
+        }
+
+
+      }
+    }
+
+    getCaught(){
+        for (let i = 0; i < rectarray.length; i++) {
+          if (this.x >= rectarray[i].x-250 && this.x <= rectarray[i].x+	250 && this.y >= rectarray[i].y-10 && this.y <=rectarray[i].y){
+            caught = true;
+            this.y = rectarray[i].y-10;
+            print("hit rectangle "+[i]+" at y value "+rectarray[i].y);
+
+          }
+          else {
+            caught = false;
+          }
 
         }
-      pop();
-      translate(0,65)
-      }
-
-  }
+    }
+}
