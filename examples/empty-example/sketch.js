093b0394406e02687e50df57760fd8e3b47a04af
lizzybrooks <lizzy.brooks@gmail.com>
2020-02-18 16:46:00 -0800

    added data API example

--- examples/empty-example/sketch.js
+++ examples/empty-example/sketch.js
@@ -1,53 +1,86 @@
-let level = localStorage.getItem("current_level");
+//create a variable to hold one ball
+let spear;
+let spearX;
+let spearY;
 
 
-function setup(){
-  createCanvas(400,400);
-  background(100,3,40);
 
-  if(localStorage){
-    if (level == null){
-      fill("white");
-      print("this is the first time the page loads. You are on level one. Push enter to increase your level.");
-    }
-    else if(level == 1){
-      fill("white");
-      print("level = "+level +". Push enter to increase your level.");
-    }
-    else if(level == 2){
-      fill("black");
-      print("level = "+level+". Push enter to increase your level.");
-    }
-
-    else {
-      fill(100,3,40);
-        print("you beat the game: no more levels. Push the right arrow to get a new game.");
-    }
-  }
-  else{
-      print("Sorry, your browser do not support local storage.");
-  }
-
-  ellipse(200,200,250,250);
+function setup() {
+  createCanvas(800, 400);
+  spear = new Spear(100,100,200,200,100,"brown", false); //make a new ball from the Ball class and call it b.
 }
 
 function draw(){
+  background(220);
 
+  spear.drawSpearTrajectory(); //draw the ball called b (go look in the Ball class for the drawBall function)
+  spear.drawCover1();
+  spear.drawCover2();
+
+}
+
+
+function mouseClicked(){
+  print(mouseX, mouseY);
+  spear.spearX = mouseX;
+  spear.spearY = mouseY;
 
 }
 
 function keyPressed(){
-  if (keyCode === 13){
-     level ++;
-    // Store
-    localStorage.setItem("current_level", level);
+        spear.moving = true;
 
-    location.reload();
-
-  }
-  if (keyCode === RIGHT_ARROW){
-    localStorage.clear();
-
-    location.reload();
-  }
 }
+
+
+
+
+class Spear {
+
+	constructor(x,y,endX,endY,coverX,color,moving){ //every ball needs an x value and a y value
+		this.x = x;
+    	this.y = y;
+
+       		this.spearX = endX;
+      		this.spearY = endY;
+
+          this.coverX = coverX;
+            this.color = color;
+        	this.moving = moving;
+	}
+
+	drawSpearTrajectory(){  // draw a ball on the screen at x,y
+
+      stroke(10);
+      fill(this.color);
+      strokeWeight(3);
+      line(this.x,this.y,this.spearX,this.spearY);
+
+
+	}
+
+  	drawCover1(){
+
+        noStroke();
+        fill(220,220,220);
+        rect(0,0,this.coverX,height);
+       if(spear.moving==true){
+            this.coverX += 2;
+      }
+  	}
+    drawCover2(){
+      if(this.spearX-this.coverX>200){
+          fill(220,220,220);
+          quad(this.coverX+200,0,this.coverX+200,height,width,height,  width,0 )
+
+      }
+    }
+
+	moveSpear(){
+    		if(this.moving == true){
+        	this.x1 = this.x1+3
+
+
+    		 }
+	}
+ }
